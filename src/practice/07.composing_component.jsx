557ed975b36deaf8e58840f561faefb2381f3799
hunterluo <hunter850711@gmail.com>
2022-06-15 01:27:12 +0800

    reusing components

---
+++ src/practice/07.composing_component.jsx
@@ -0,0 +1,13 @@
+import Hello from "./components/06.hello"
+
+const Composing = () => {
+    return (
+        <div>
+            <Hello name="Frank" />
+            <Hello name="David" />
+            <Hello name="Alex" />
+        </div>
+    )
+}
+
+export default Composing
