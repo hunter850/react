76c039019619242a3ad98a4490215be0f1279497
hunterluo <hunter850711@gmail.com>
2022-06-15 01:33:49 +0800

    change component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/home/index.jsx'
-// import Attr from './practice/03.jsx_attribute.jsx'
+// import Home from './pages/home/index.jsx'
+import Composing from './practice/07.composing_component'
 import './index.css'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
     <React.StrictMode>
-        <Home />
+        <Composing />
     </React.StrictMode>
 )
