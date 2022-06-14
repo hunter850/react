a3c84edc3187a2b516b8ca34d244323629383ff9
hunter850 <hunter850711@gmail.com>
2022-06-14 12:55:41 +0800

    Home tag to JsxExp tag

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/home/index.jsx'
+// import Home from './pages/home/index.jsx'
+import JsxExp from './practice/02.jsx_expression'
 import './index.css'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
     <React.StrictMode>
-        <Home />
+        <JsxExp />
     </React.StrictMode>
 )
