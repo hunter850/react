b98134db0e431d953b1d31d970a4ae3871f3a77d
hunter850 <hunter850711@gmail.com>
2022-06-15 09:02:35 +0800

    donwload uuid

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import Home from './pages/home/index.jsx'
-import Composing from './practice/07.composing_component'
+import ForLoop from './practice/04.for.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
     <React.StrictMode>
-        <Composing />
+        <ForLoop />
     </React.StrictMode>
 )
