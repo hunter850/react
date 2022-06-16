03bb1585426dada96f3e24e9ae921b96ef461c12
hunter850 <hunter850711@gmail.com>
2022-06-16 16:36:05 +0800

    InlineIfElse

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import Home from './pages/home/index.jsx'
-import InlineIf from './practice/15.inline_if'
+import InlineIfElse from './practice/16.inline_if_else'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <React.StrictMode>
-        <InlineIf />
+        <InlineIfElse />
     </React.StrictMode>
 )
