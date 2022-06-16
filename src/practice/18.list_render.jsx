f5a4baec39af61b84d1fe4ca17e1ca95135761bb
hunterluo <hunter850711@gmail.com>
2022-06-16 23:17:00 +0800

    list render & key

--- src/practice/18.list_render.jsx
+++ src/practice/18.list_render.jsx
@@ -1,10 +1,18 @@
 import NumberList from "./components/18.number_list"
+import ComponentLi from "./components/18.component_li"
+import { v4 } from "uuid"
 
 const ListRender = () => {
     const numbers = [1, 2, 3, 4];
 
     const el = (
-        <NumberList numbers={numbers} />
+        <div>
+            <NumberList numbers={numbers} />
+            <ul>
+                //如果要做list render 誰被複製就要把key綁在誰身上 而不是綁在component裡的li
+                {numbers.map(number => <ComponentLi number={number} key={v4()} />)}
+            </ul>
+        </div>
     )
 
     return el;
---
+++ src/practice/components/18.component_li.jsx
@@ -0,0 +1,8 @@
+const ComponentLi = ({ number }) => {
+    const el =(
+        <li>{number}</li>
+    )
+    return el;
+}
+
+export default ComponentLi
