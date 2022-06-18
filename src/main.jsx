8dae761dee2b53b3d991262f14e3389ca77ac9d2
hunterluo <hunter850711@gmail.com>
2022-06-19 01:04:39 +0800

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import Home from './pages/home/index.jsx'
-import MultiInput from './practice/24.multi_input'
+import TryUseEffect from './practice/28.useEffect'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <React.StrictMode>
-        <MultiInput />
-    </React.StrictMode>
+        <TryUseEffect />
 )
