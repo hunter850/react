01f04c7999eb53ede7af4c427481646ffb93773b
hunter850 <hunter850711@gmail.com>
2022-06-20 15:22:52 +0800

    didMount willUnmount

--- src/practice/components/31.component_false.jsx
+++ src/practice/components/31.component_false.jsx
@@ -1,17 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef, Fragment } from "react";
 
 const ComponentFalse = () => {
 
+    const initialed = useRef(false);
+    const [count, setCount] = useState(0);
+
+    const clickHandler = () => {
+        setCount(pre => pre + 1);
+    }
+
     useEffect(() => {
-        console.log("false mount");
+        if(!initialed.value) {
+            initialed.value = true;
+            console.log("false mount");
+        } else {
+            console.log("false update");
+        }
 
         return () => {
             console.log("false unmout");
         }
-    }, []);
+    }, [count]);
 
     const el = (
-        <h1>False</h1>
+        <Fragment>
+            <h1>False</h1>
+            <h2>update {count} times</h2>
+            <button onClick={clickHandler}>click to update</button>
+            <br />
+            <br />
+        </Fragment>
     )
 
     return el;
--- src/practice/components/31.component_true.jsx
+++ src/practice/components/31.component_true.jsx
@@ -1,17 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef, Fragment } from "react";
 
 const ComponentTrue = () => {
 
+    const initialed = useRef(false);
+    const [count, setCount] = useState(0);
+
+    const clickHandler = () => {
+        setCount(pre => pre + 1);
+    }
+
     useEffect(() => {
-        console.log("true mount");
+        if(!initialed.value) {
+            initialed.value = true;
+            console.log("true mount");
+        } else {
+            console.log("true update");
+        }
 
         return () => {
             console.log("true unmout");
         }
-    }, []);
+    }, [count]);
 
     const el = (
-        <h1>True</h1>
+        <Fragment>
+            <h1>True</h1>
+            <h2>update {count} times</h2>
+            <button onClick={clickHandler}>click to update</button>
+            <br />
+            <br />
+        </Fragment>
     )
 
     return el;
