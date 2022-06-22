e7a84d2fa6df05eda2a78feb387c568a7562112f
hunter850 <hunter850711@gmail.com>
2022-06-22 16:52:54 +0800

    transition

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import Home from './pages/home/index.jsx'
-import Mount from './practice/31.mount'
+import Transition from './practice/32.transition'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-        <Mount />
+        <Transition />
 )
