2878b8e79d5f22d600808aa377179408ba7e73e6
hunterluo <hunter850711@gmail.com>
2022-06-28 19:05:24 +0800

    create useGSAPScroll

--- src/practice/TryUseHook.jsx
+++ src/practice/TryUseHook.jsx
@@ -1,14 +1,14 @@
 import { Fragment } from "react"
-import useScrollTo from "./hooks/useScrollTo";
+import useGSAPScroll from "./hooks/useGSAPScroll";
 
 const TryUseHook = () => {
 
-    const scroll = useScrollTo();
+    const scroll = useGSAPScroll();
 
     const el = (
         <Fragment>
             <div style={{height: "500vh"}}></div>
-            <button style={{position: "fixed", left: "20px", bottom: "20px"}} onClick={() => scroll(0)}>click to hide</button>
+            <button style={{position: "fixed", left: "20px", bottom: "20px"}} onClick={() => scroll(0, 1, "power2.out")}>click to hide</button>
         </Fragment>
     )
 
