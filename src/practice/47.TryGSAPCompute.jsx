b1bd9be803b7c40e1c22cc167c691ab3a475d9bb
hunterluo <hunter850711@gmail.com>
2022-07-02 14:17:02 +0800

    try seAnimation

--- src/practice/47.TryGSAPCompute.jsx
+++ src/practice/47.TryGSAPCompute.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState, useEffect } from "react"
 import useGSAPCompute from "./hooks/useGSAPCompute"
+import useSetAnimation from "./hooks/useSetAnimation";
 
 const TryGSAPCompute = () => {
 
@@ -7,6 +8,7 @@ const TryGSAPCompute = () => {
     const [content, setContent] = useState({ price: 0 });
     const [boxStyle, setBoxStyle] = useState({width: "200px", height: "200px", backgroundColor: "#faf"})
     const compute = useGSAPCompute();
+    const setAnimation = useSetAnimation();
 
     const inputHandler = (event) => {
         setInputValue(parseInt(event.target.value || 0));
@@ -14,6 +16,9 @@ const TryGSAPCompute = () => {
 
     useEffect(() => {
         compute({ price: inputValue }, content, setContent, 0.5);
+        setAnimation(() => {
+            console.log("set")
+        }, 1000);
     }, [inputValue]);
 
     const el = (
