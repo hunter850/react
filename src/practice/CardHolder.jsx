ea202e99f61b3e71b0138cf1435c4ce0729a50cd
hunterluo <hunter850711@gmail.com>
2022-07-07 00:28:53 +0800

    card holder

--- src/practice/CardHolder.jsx
+++ src/practice/CardHolder.jsx
@@ -1,56 +1,56 @@
-import { Fragment, useState, useEffect, useMemo } from "react"
+import { Fragment, useState, useEffect, useRef, useMemo } from "react"
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { v4 } from "uuid";
-import usePrevious from "./hooks/usePrevious"
+import useIndexArray from "./hooks/useIndexArray";
 import "./css/cardNumberInput.css"
 
 const CardHolder = ({ cardName }) => {
 
-    // const cardMask = useMemo(() => {
-    //     return cardName.split("").map((item, index) => ({index}))
-    // }, [cardName])
-
-    const [cardMask, setCardMask] = useState([
-        { content: "#", index: 0 },
-        { content: "#", index: 1 },
-        { content: "#", index: 2 },
-        { content: "#", index: 3 },
-        { content: "#", index: 4 },
-        { content: "#", index: 5 },
-        { content: "#", index: 6 },
-        { content: "#", index: 7 },
-        { content: "#", index: 8 },
-        { content: "#", index: 9 },
-        { content: "#", index: 10 },
-        { content: "#", index: 11 },
-        { content: "#", index: 12 },
-        { content: "#", index: 13 },
-        { content: "#", index: 14 },
-        { content: "#", index: 15 },
-    ])
-
+    const cardMask = useIndexArray(100);
     const [tempArray, setTempArray] = useState([]);
+    const [contentWidth, setContentWidth] = useState(0);
+    const [textHide, setTextHide] = useState(false);
+    const nameRef = useRef(null);
+    const nameSplit = useMemo(() => cardName.split(""), [cardName]);
+    const nameLength = useMemo(() => cardName.split("").length, [cardName]);
 
     useEffect(() => {
+        //紀錄transition leave時候的content
         setTempArray(pre => {
+            const output = [...pre];
             let tempName = cardName.toUpperCase();
-            pre.splice(0, tempName.split("").length, ...tempName.split(""));
-            return [...pre];
+            output.splice(0, tempName.split("").length, ...tempName.split(""));
+            return output;
         });
     }, [cardName]);
 
+    useEffect(() => {
+        //name總長超過220就overflow hidden並且顯示...
+        const contents = nameRef.current.childNodes[0].childNodes;
+        let widthTotal = 0;
+        for(let i = 0; i < cardName.length; i++) {
+            widthTotal += (parseFloat(getComputedStyle(contents[i]).width) || 0);
+        }
+        if(widthTotal < 220) {
+            setContentWidth(widthTotal);
+            setTextHide(false);
+        } else {
+            setTextHide(true);
+        }
+    }, [cardName])
+
     const el = (
         <Fragment>
-            <div className="card_name" style={{height: "32px"}}>
-                <TransitionGroup className="card_name_split">
+            <div className="card_name" style={{height: "32px", display: "flex"}} ref={nameRef}>
+                <TransitionGroup className={`card_name_split ${textHide ? "hide_text" : ""}`} style={{maxWidth: `${contentWidth}px`}}>
                     {cardMask.map(item => (
                         <Fragment key={item.index}>
-                            <CSSTransition in={item.index < cardName.split("").length} timeout={500} classNames="text_span right">
-                                <span className="text_span">{cardName.split("")[item.index] === undefined ? tempArray[item.index] : cardName.split("")[item.index].toUpperCase()}</span>
+                            <CSSTransition in={item.index < nameLength} timeout={500} classNames="text_span right" unmountOnExit>
+                                <span className={`text_span ${nameSplit[item.index] === " " ? "space_width" : ""}`}>{nameSplit[item.index] === undefined ? tempArray[item.index] : nameSplit[item.index].toUpperCase()}</span>
                             </CSSTransition>
                         </Fragment>
                     ))}
                 </TransitionGroup>
+                {textHide ? <span style={{marginLeft: "3px"}}>...</span> : ""}
             </div>
         </Fragment>
     )
