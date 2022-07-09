78786beae02f711ef77170ada366f06b8b68f58a
hunterluo <hunter850711@gmail.com>
2022-07-09 14:22:07 +0800

    CreditCardContainter prettier

--- src/practice/CreditcardContainter.jsx
+++ src/practice/CreditcardContainter.jsx
@@ -1,35 +1,30 @@
 import { Fragment } from "react";
 import Creditcard from "./Creditcard";
 
-const CreditcarContainer = () => {
-    const el = (
+function CreditcarContainer() {
+    const fakeBodyStyle = {
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#FCFAF7",
+    };
+    const fakeNav = {
+        width: "100%",
+        height: "60px",
+        backgroundColor: "#253945",
+        marginBottom: "50px",
+    };
+    const containter = { padding: "0px 200px" };
+
+    return (
         <Fragment>
-            <div
-                className="fake_body"
-                style={{
-                    width: "100vw",
-                    height: "100vh",
-                    backgroundColor: "#FCFAF7",
-                }}
-            >
-                <div
-                    style={{
-                        width: "100%",
-                        height: "60px",
-                        backgroundColor: "#253945",
-                        marginBottom: "50px",
-                    }}
-                >
-                    nav
-                </div>
-                <div style={{ padding: "0px 200px" }}>
+            <div className="fake_body" style={fakeBodyStyle}>
+                <div style={fakeNav}>nav</div>
+                <div style={containter}>
                     <Creditcard />
                 </div>
             </div>
         </Fragment>
     );
-
-    return el;
-};
+}
 
 export default CreditcarContainer;
