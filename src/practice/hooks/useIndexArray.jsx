55360a9b19e9ae2304f30818d40215ae5e43932d
hunterluo <hunter850711@gmail.com>
2022-07-10 02:41:52 +0800

    修正useIndexArray改用useMemo計算

--- src/practice/hooks/useIndexArray.jsx
+++ src/practice/hooks/useIndexArray.jsx
@@ -1,15 +1,18 @@
-
+import { useMemo } from "react";
 
 const useIndexArray = (num, ...items) => {
-    const tempArray = [];
-    for(let i = 0; i < num; i++) {
-        const tempObj = {index: i};
-        items.forEach((item, index) => {
-            tempObj[`content${index}`] = item + "" + i;
-        })
-        tempArray.push(tempObj);
-    }
-    return tempArray;
+    const output = useMemo(() => {
+        const tempArray = [];
+        for(let i = 0; i < num; i++) {
+            const tempObj = {index: i};
+            items.forEach((item, index) => {
+                tempObj[`content${index}`] = item + "" + i;
+            })
+            tempArray.push(tempObj);
+        }
+        return tempArray;
+    }, [num, ...items]);
+    return output;
 }
 
 export default useIndexArray
