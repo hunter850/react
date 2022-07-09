953aa4df38c601503c7a39cbfc1177fe0b0e664c
hunterluo <hunter850711@gmail.com>
2022-07-09 14:11:47 +0800

    add eslint prettier

--- src/practice/CreditcardContainter.jsx
+++ src/practice/CreditcardContainter.jsx
@@ -1,19 +1,35 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
 import Creditcard from "./Creditcard";
 
 const CreditcarContainer = () => {
     const el = (
         <Fragment>
-            <div className="fake_body" style={{width: "100vw", height: "100vh",  backgroundColor: "#FCFAF7"}}>
-                <div style={{ width: "100%", height: "60px", backgroundColor: "#253945", marginBottom: "50px" }}>nav</div>
+            <div
+                className="fake_body"
+                style={{
+                    width: "100vw",
+                    height: "100vh",
+                    backgroundColor: "#FCFAF7",
+                }}
+            >
+                <div
+                    style={{
+                        width: "100%",
+                        height: "60px",
+                        backgroundColor: "#253945",
+                        marginBottom: "50px",
+                    }}
+                >
+                    nav
+                </div>
                 <div style={{ padding: "0px 200px" }}>
                     <Creditcard />
                 </div>
             </div>
         </Fragment>
-    )
+    );
 
     return el;
-}
+};
 
-export default CreditcarContainer
+export default CreditcarContainer;
