f76abfb27d4da6498055afbd40965e9d12c1cbfc
hunterluo <hunter850711@gmail.com>
2022-07-16 21:19:44 +0800

    create useIsIn

---
+++ src/practice/hooks/useIsIn.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const useIsIn = (ref, num = 0) => {
+    const [isIn, setIsIn] = useState(false);
+    useEffect(() => {
+        if (ref.current) {
+            if (
+                ref.current.getBoundingClientRect().top -
+                    window.innerHeight +
+                    num <=
+                0
+            ) {
+                setIsIn(true);
+            } else {
+                setIsIn(false);
+            }
+        }
+    }, []);
+    useEffect(() => {
+        function check() {
+            if (ref.current) {
+                if (
+                    ref.current.getBoundingClientRect().top -
+                        window.innerHeight +
+                        num <=
+                    0
+                ) {
+                    setIsIn(true);
+                } else {
+                    setIsIn(false);
+                }
+            }
+        }
+        window.addEventListener("scroll", check);
+        return () => window.removeEventListener("scroll", check);
+    }, [ref, num]);
+    return isIn;
+};
+
+export default useIsIn;
